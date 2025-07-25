5abdf6ba27233609a6d4683ac2cbb43951194172
A.Raja <rajaanthonysamy5798@gmail.com>
2025-07-25 22:47:56 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MedicalChatbot',
+    version='0.1',
+    author='Raja',
+    author_email="rajaanthonysamy4798@gmail.com",
+    packages=find_packages(),
+    install_requires=[])
---
+++ template.sh
@@ -0,0 +1,15 @@
+mkdir -p src
+mkdir -p research
+
+
+touch src/__init__.py
+touch research/__init__.py
+touch src/prompt.py
+touch .env
+touch app.py
+touch setup.py
+touch research/trails.ipynb
+touch requirements.txt
+
+
+echo "All files are created successfully"
